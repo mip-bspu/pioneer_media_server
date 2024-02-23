e02534b09fc24514293101b315b66952da3b76b0
RodionLV <arknim@mail.ru>
2024-02-23 10:45:24 +0900

    add: seeds for Content.File

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,75 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias MediaServer.Repo
+alias MediaServer.Content
+alias MediaServer.Util.TimeUtil
+
+if Mix.env() == :dev do
+
+  if Repo.all(Content.File) == [] do
+    date = TimeUtil.current_date_time()
+
+    %Content.File{}
+    |> Content.File.changeset(%{
+      uuid: Ecto.UUID.generate(),
+      date_create: date,
+      extention: ".pdf",
+      name: "content 1",
+      tags: %{
+        0 => %{name: "city"}
+      }
+    })
+    |> Repo.insert!()
+
+    %Content.File{}
+    |> Content.File.changeset(%{
+      uuid: Ecto.UUID.generate(),
+      date_create: date,
+      extention: ".pdf",
+      name: "content 2",
+      tags: %{
+        0 => %{name: "school"},
+        1 => %{name: "city"}
+      }
+    })
+    |> Repo.insert!()
+
+    %Content.File{}
+    |> Content.File.changeset(%{
+      uuid: Ecto.UUID.generate(),
+      date_create: date,
+      extention: ".pdf",
+      name: "content 3",
+      tags: %{
+        0 => %{name: "school"},
+      }
+    })
+    |> Repo.insert!()
+
+    %Content.File{}
+    |> Content.File.changeset(%{
+      uuid: Ecto.UUID.generate(),
+      date_create: Timex.shift(date, months: -1),
+      extention: ".pdf",
+      name: "content 4",
+      tags: %{
+        0 => %{name: "school"},
+      }
+    })
+    |> Repo.insert!()
+
+    %Content.File{}
+    |> Content.File.changeset(%{
+      uuid: Ecto.UUID.generate(),
+      date_create: Timex.shift(date, months: -2),
+      extention: ".pdf",
+      name: "content 5",
+      tags: %{
+        0 => %{name: "school"},
+      }
+    })
+    |> Repo.insert!()
+  end
+end
